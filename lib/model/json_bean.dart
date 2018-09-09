c53fd8b07acc49e3f23a240c1c9f4deb113d5255
wenyangw <wenyangw@gmail.com>
2018-09-09 17:35:51 +0800

    读取服务器端数据优化

--- lib/model/json_bean.dart
+++ lib/model/json_bean.dart
@@ -4,12 +4,13 @@ import 'package:jxc/model/obj_bean.dart';
 part 'json_bean.g.dart';
 
 @JsonSerializable()
-class JsonBean extends Object with _$JsonBeanSerializerMixin{
-  JsonBean(this.msg, this.success, this.obj);
-
+class JsonBean extends Object{
   String msg;
   bool success;
   ObjBean obj;
 
+  JsonBean(this.msg, this.success, this.obj);
+
   factory JsonBean.fromJson(Map<String, dynamic> json) => _$JsonBeanFromJson(json);
+  Map<String, dynamic> toJson() => _$JsonBeanToJson(this);
 }
--- lib/model/obj_bean.dart
+++ lib/model/obj_bean.dart
@@ -3,11 +3,12 @@ import 'package:json_annotation/json_annotation.dart';
 part 'obj_bean.g.dart';
 
 @JsonSerializable()
-class ObjBean extends Object with _$ObjBeanSerializerMixin{
+class ObjBean extends Object{
   ObjBean(this.userName, this.realName);
 
   String userName;
   String realName;
 
   factory ObjBean.fromJson(Map<String, dynamic> json) => _$ObjBeanFromJson(json);
+  Map<String, dynamic> toJson() => _$ObjBeanToJson(this);
 }
